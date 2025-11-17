b269bfa4ec9d2236aaed69fd93e6565582711ab7
abdulrehman <abdulrehmanseelro059@gmail.com>
2025-11-17 13:44:12 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+//          javascript
+//  two types of js 
+//  1)vanilla = old (var)
+//  2)ES6(ECMA Script 6) = new (let , const) 
+// Using var to declare a variable (vanilla , old way)
+// var = variable , name = identifier , value = data
+// reassign and redeclare were allowed in var
+var name = "rehman";
+document.write(name);
+// using let to declare a variable (ES6 , new way)
+// rassign is allowed but redeclare is not allowed in let
+let x = "hello world";
+document.write(x);
+// using const to declare a variable (ES6 , new way)
+// reassign and redeclare are not allowed in const
+const a = ("hi everyone");
+document.write(a);
+console.log("hello world!");
